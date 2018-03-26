239a94e595c174c55150dc70743847ac52b56ade
Alan Campbell <alancampbell4444@gmail.com>
2018-03-26 09:29:21 -0700

    s

--- app/components/Reservation.jsx
+++ app/components/Reservation.jsx
@@ -27,14 +27,16 @@ export default class Reservation extends React.Component {
       <div id='reserveBody'>
         <div id='reserveCanvas'>
         <h1 id='reserveTitle'>Book online and save $10-<br/> on the 800' Flight!<br/>  </h1>
-       <p id='contactDetails'> * Must reserve 24 hours in advance. After you make a reservation, we will contact you to arrange pick-up/drop-off details. </p> <br/>
+       <p id='openDate'>     Open April 20th, 2018 </p>
+       <p id='contactDetails'> * If your desired reservation time is less than 24 hours away, call us to book. Otherwise, feel free to book online. If you make an online reservation, we will contact you to arrange pick-up/drop-off details.  </p>
       
-
+     <div id='bCW'>
               <iframe id="bookingCalendar" 
               src="https://secure.webreserv.com/services/bookingcalendar.do?businessid=havasuparasailaz&embedded=y&search=0&avgrid=n&css=/assets/css/bookingcalendar-2.0/theme-grey-red.css"                      frameBorder="0"
               >
                 <a href="https://secure.webreserv.com/services/bookingcalendar.do?businessid=havasuparasailaz&embedded=y&search=0&avgrid=n&css=/assets/css/bookingcalendar-2.0/theme-grey-red.css">Make Reservation</a>
               </iframe>
+      </div>
             
         </div>
       </div>
